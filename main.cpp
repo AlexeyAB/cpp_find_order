06a80340ff403c4693bfe0ff8b80584f029c71a3
AlexeyAB <kikots@mail.ru>
2013-06-04 22:31:58 +0400

    C++ optimization 2^N, by using unrolling of templates.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>     /* calloc, exit, free */
 #include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
 
+#include <memory>		/* std::unique_ptr<> */
+#include <array>		/* std::array<> */
+
 const size_t c_array_size = 10000000;
 
 /* Fields */
@@ -33,13 +36,105 @@ static inline struct T_cash_account_row generate_row() {
 struct T_range_filters {
     struct T_cash_account_row begin, end;
     /* bytes array or bitset from https://gist.github.com/jmbr/667605 */
-    unsigned char use_filter[last_e]; 
+	unsigned char use_filter[last_e]; 
 };
-/* ----------------------------------------------------------------------- */
+// -------------------------------------------------------------------------
+
+// The templated constructor of unrolling of the class (no return, mandatory call to the constructor, called once)
+template<unsigned unroll_count, template<unsigned> class T>
+struct T_unroll_constructor {
+	T_unroll_constructor<unroll_count-1, T> next_unroll;
+	T<unroll_count-1> functor;
+	template<typename T1> inline T_unroll_constructor(T1 & val1) : next_unroll(val1), functor(val1) {}
+};
+// End of unroll
+template<template<unsigned> class T>
+struct T_unroll_constructor<0, T> { 
+	template<typename T1> inline T_unroll_constructor(T1 &) {} 
+};
+// -------------------------------------------------------------------------
+
+// Abstract base class of filters for each search variant (range_filters)
+struct T_filter {
+	// search
+	virtual size_t search(T_cash_account_row const*const __restrict array_ptr, const size_t c_array_size,
+		T_cash_account_row *const __restrict result_ptr, T_range_filters const*const __restrict range_filters) = 0;
+};
+// -------------------------------------------------------------------------
+
+// The filters for each search variant (range_filters)
+template<unsigned index_pred>
+struct T_custom_filter : T_filter {
+
+	inline unsigned char test_predicate(T_cash_account_row const*const __restrict row, 
+		T_range_filters const*const __restrict range_filters) 
+	{    
+		return 
+			(!(index_pred & 1<<amount_of_money_e) || 
+				(row->amount_of_money >= range_filters->begin.amount_of_money &&
+				row->amount_of_money <= range_filters->end.amount_of_money)) &&
+			(!(index_pred & 1<<gender_e) || 
+				(row->gender >= range_filters->begin.gender && row->gender <= range_filters->end.gender)) &&
+			(!(index_pred & 1<<age_e) || 
+				(row->age >= range_filters->begin.age && row->age <= range_filters->end.age)) &&
+			(!(index_pred & 1<<code_e) || 
+				(row->code >= range_filters->begin.code && row->code <= range_filters->end.code)) &&
+			(!(index_pred & 1<<height_e) || 
+				(row->height >= range_filters->begin.height && row->height <= range_filters->end.height));
+	}
+	// -------------------------------------------------------------------------
+
+	// search
+	virtual size_t search(T_cash_account_row const*const __restrict array_ptr, const size_t c_array_size,
+		T_cash_account_row *const __restrict result_ptr, T_range_filters const*const __restrict range_filters) final
+	{
+		size_t result_size = 0;
+		size_t i; // loop index
+		for(i = 0; i < c_array_size; ++i) {
+			if(test_predicate(array_ptr + i, range_filters)) 
+				result_ptr[result_size] = array_ptr[i], ++result_size;
+		}
+		return result_size;
+	}
+};
+// -------------------------------------------------------------------------
+
+class T_optimized_search {
+	// unroll tamplates
+	template<unsigned index_pred>
+	struct T_unroll_find {
+		template<typename T> T_unroll_find(T &filters) { 
+			filters[index_pred].reset( new T_custom_filter<index_pred>() ); 
+		}
+	};
+	// -------------------------------------------------------------------------
+
+	// Get index of T_test_pred version for current search range
+	inline unsigned get_index_pred(T_range_filters const*const __restrict range_filters) {
+		unsigned result = 0;
+		for(size_t i = 0; i < last_e; ++i) 
+			result |= range_filters->use_filter[i]?(1<<i):0;
+		return result;
+	}
+
+	std::array<std::unique_ptr<T_filter>, 1<<last_e> filters;
+	T_unroll_constructor<1<<last_e, T_unroll_find> fill_filter;
+public:
+	T_optimized_search() : fill_filter(filters) {}
+
+	// C++ optimized search
+	inline size_t search(T_cash_account_row const*const __restrict array_ptr, const size_t c_array_size,
+			T_cash_account_row *const __restrict result_ptr, T_range_filters const*const __restrict range_filters) 
+	{
+		auto const& filter = filters[get_index_pred(range_filters)];
+		return filter->search(array_ptr, c_array_size, result_ptr, range_filters);
+	}
+};
+// -------------------------------------------------------------------------
 
 /* Compare row with filters */
-static inline unsigned char test_predicate(struct T_cash_account_row const*const row, 
-    struct T_range_filters const*const range_filters) 
+static inline unsigned char test_predicate(struct T_cash_account_row const*const __restrict row, 
+    struct T_range_filters const*const __restrict range_filters) 
 {    
     return 
         (!range_filters->use_filter[amount_of_money_e] || 
@@ -57,8 +152,8 @@ static inline unsigned char test_predicate(struct T_cash_account_row const*const
 /* ----------------------------------------------------------------------- */
 
 /* search */
-static inline size_t search(struct T_cash_account_row const*const array_ptr, const size_t c_array_size,
-	struct T_cash_account_row *const result_ptr, struct T_range_filters const*const range_filters) 
+static inline size_t search(struct T_cash_account_row const*const __restrict array_ptr, const size_t c_array_size,
+	struct T_cash_account_row *const __restrict result_ptr, struct T_range_filters const*const __restrict range_filters) 
 {
 	size_t result_size = 0;
 	size_t i; /* loop index */
@@ -66,7 +161,7 @@ static inline size_t search(struct T_cash_account_row const*const array_ptr, con
 		if(test_predicate(array_ptr + i, range_filters)) 
 			result_ptr[result_size] = array_ptr[i], ++result_size;
 	}
-	return result_size; 
+	return result_size;
 }
 /* ----------------------------------------------------------------------- */
 
@@ -87,7 +182,7 @@ int main() {
 		array_ptr[i] = generate_row();
 	printf ("Generated rows: %d \n", c_array_size);
 
-    struct T_range_filters range_filters = {};
+    struct T_range_filters range_filters = {0};  
 
 	range_filters.use_filter[amount_of_money_e] = rand()%1 + 0;
 	range_filters.use_filter[gender_e] = rand()%1 + 0;
@@ -105,21 +200,34 @@ int main() {
 
 	size_t result_size;
 	clock_t end, start;
+	T_optimized_search optimized_search;	// C++ optimized search
+
+	printf ("C++-Searching...\n");
+	
+	start = clock();
+	result_size = optimized_search.search(array_ptr, c_array_size, result_ptr, &range_filters);
+	end = clock();
+	float cpp_took_time = static_cast<float>(end - start);
+	printf ("C++-optimized search took %f seconds.\n", cpp_took_time/CLOCKS_PER_SEC);
+
+	printf ("Found rows: %d \n", result_size);
 
 	printf ("C-Searching...\n");
 
 	start = clock();
 	result_size = search(array_ptr, c_array_size, result_ptr, &range_filters);
 	end = clock();
-	float c_took_time = (float)(end - start);
+	float c_took_time = static_cast<float>(end - start);
 	printf ("C-search took %f seconds.\n", c_took_time/CLOCKS_PER_SEC);
 
+	printf ("The C++ faster than C: %f times \n", c_took_time/cpp_took_time);
+
 	printf ("Found rows: %d \n", result_size);
-    
-	/*for(i = 0; i < result_size; ++i) {
-		printf ("%d, %d, %d, %d, %d \n", 
-			result_ptr[i].age, result_ptr[i].amount_of_money, result_ptr[i].code, result_ptr[i].gender, result_ptr[i].height);
-	}*/
+   
+	//for(i = 0; i < result_size; ++i) {
+	//	printf ("%d, %d, %d, %d, %d \n", 
+	//		result_ptr[i].age, result_ptr[i].amount_of_money, result_ptr[i].code, result_ptr[i].gender, result_ptr[i].height);
+	//}
 
     int qwerty;
     scanf ("%d",&qwerty);
--- test_pradicate_cpp.asm
+++ test_pradicate_cpp.asm
@@ -1,68 +1,35 @@
-		if(test_predicate(array_ptr + i, range_filters)) 
-000000013F6211A3  mov         eax,dword ptr [r9+0Ch]  
-000000013F6211A7  and         eax,0FFFFFh  
-000000013F6211AC  cmp         ecx,eax  
-000000013F6211AE  ja          search+10Fh (013F62127Fh)  
-000000013F6211B4  cmp         byte ptr [r9+11h],0  
-000000013F6211B9  je          search+78h (013F6211E8h)  
-000000013F6211BB  mov         ecx,dword ptr [r10+rdx*8]  
-000000013F6211BF  mov         eax,dword ptr [r9]  
-000000013F6211C2  shr         ecx,14h  
-000000013F6211C5  shr         eax,14h  
-000000013F6211C8  and         ecx,1  
-000000013F6211CB  and         eax,1  
-000000013F6211CE  cmp         ecx,eax  
-000000013F6211D0  jb          search+10Fh (013F62127Fh)  
-000000013F6211D6  mov         eax,dword ptr [r9+8]  
-000000013F6211DA  shr         eax,14h  
-000000013F6211DD  and         eax,1  
-000000013F6211E0  cmp         ecx,eax  
-000000013F6211E2  ja          search+10Fh (013F62127Fh)  
+			if(test_predicate(array_ptr + i, range_filters)) 
+
 // !range_filters->use_filter[age_e] ||  
-000000013F6211E8  cmp         byte ptr [r9+12h],0  
-000000013F6211ED  je          search+0A4h (013F621214h)  
+// Optimized and reduced: !range_filters->use_filter[age_e] ||  
+// 
 // (row->age >= range_filters->begin.age && row->age <= range_filters->end.age)
-000000013F6211EF  mov         ecx,dword ptr [r10+rdx*8]  
-000000013F6211F3  mov         eax,dword ptr [r9] 
-
- 
-000000013F6211F6  shr         ecx,15h  
-000000013F6211F9  shr         eax,15h  
-000000013F6211FC  and         ecx,7Fh  
-000000013F6211FF  and         eax,7Fh  
-000000013F621202  cmp         ecx,eax  
-000000013F621204  jb          search+10Fh (013F62127Fh)  
-000000013F621206  mov         eax,dword ptr [r9+8]  
-
-000000013F62120A  shr         eax,15h  
-000000013F62120D  and         eax,7Fh  
-000000013F621210  cmp         ecx,eax  
-000000013F621212  ja          search+10Fh (013F62127Fh)  
-000000013F621214  cmp         byte ptr [r9+13h],0  
-000000013F621219  je          search+0CEh (013F62123Eh)  
-000000013F62121B  mov         ecx,dword ptr [r10+rdx*8]  
-000000013F62121F  mov         eax,dword ptr [r9]  
+000000013FE03520  mov         edx,dword ptr [r14+r10*8]  
+000000013FE03524  mov         r9d,dword ptr [rdi]  
+000000013FE03527  mov         r8d,edx  // save: edx
+000000013FE0352A  mov         eax,r9d  // save: r9d
+000000013FE0352D  shr         r8d,15h  
+000000013FE03531  shr         eax,15h  
+000000013FE03534  and         r8d,7Fh  
+000000013FE03538  and         eax,7Fh  
+000000013FE0353B  cmp         r8d,eax  
+000000013FE0353E  jb          T_custom_filter<12>::search+88h (013FE03578h)  
+000000013FE03540  mov         ecx,dword ptr [rdi+8]  
+000000013FE03543  mov         eax,ecx  
+000000013FE03545  shr         eax,15h  
+000000013FE03548  and         eax,7Fh  
+000000013FE0354B  cmp         r8d,eax  
+000000013FE0354E  ja          T_custom_filter<12>::search+88h (013FE03578h)  
+// Optimized and reduced: !range_filters->use_filter[code_e] ||  
+// 
+// No need to load: edx, r9d (C-edition: ecx, eax), because they were been saved
+//
 // (row->code >= range_filters->begin.code && row->code <= range_filters->end.code)
-000000013F621222  and         ecx,0FFFFFh  
-000000013F621228  and         eax,0FFFFFh  
-000000013F62122D  cmp         ecx,eax  
-000000013F62122F  jb          search+10Fh (013F62127Fh)  
-000000013F621231  mov         eax,dword ptr [r9+8]  
-000000013F621235  and         eax,0FFFFFh  
-000000013F62123A  cmp         ecx,eax  
-000000013F62123C  ja          search+10Fh (013F62127Fh)  
-000000013F62123E  cmp         byte ptr [r9+14h],0  
-000000013F621243  je          search+103h (013F621273h)  
-000000013F621245  mov         ecx,dword ptr [r10+rdx*8+4]  
-000000013F62124A  mov         eax,dword ptr [r9+4]  
-000000013F62124E  shr         ecx,14h  
-000000013F621251  shr         eax,14h  
-000000013F621254  and         ecx,1FFh  
-000000013F62125A  and         eax,1FFh  
-000000013F62125F  cmp         ecx,eax  
-000000013F621261  jb          search+10Fh (013F62127Fh)  
-000000013F621263  mov         eax,dword ptr [r9+0Ch]  
-000000013F621267  shr         eax,14h  
-000000013F62126A  and         eax,1FFh  
-000000013F62126F  cmp         ecx,eax  
-000000013F621271  ja          search+10Fh (013F62127Fh)  
+000000013FE03550  and         edx,0FFFFFh  
+000000013FE03556  and         r9d,0FFFFFh  
+000000013FE0355D  cmp         edx,r9d  
+000000013FE03560  jb          T_custom_filter<12>::search+88h (013FE03578h)  
+// 
+000000013FE03562  and         ecx,0FFFFFh  
+000000013FE03568  cmp         edx,ecx  
+000000013FE0356A  ja          T_custom_filter<12>::search+88h (013FE03578h)  
